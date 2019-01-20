70fa5ec7fb64afeaa9387c6e1bf907cfb831bd85
孙玉龙 <sunyulong@zxiat.org>
2019-01-20 18:05:15 +0800

    优化登录界面，添加滑块验证

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NbLoginComponent } from '@nebular/auth';
+
+declare var SlidingVerificationCode: any;
+import $ from "jquery";
 
 @Component({
   selector: 'ngx-login',
@@ -7,12 +10,33 @@ import { NbLoginComponent } from '@nebular/auth';
   styleUrls: ['./login.component.scss']
 
 })
-export class LoginComponent extends NbLoginComponent {
+export class LoginComponent extends NbLoginComponent implements OnInit {
+
+
+  public inits = function () {
+    var eles = ($('div:hidden').find('#captcha').length > 0) ? $('div:hidden').find('#captcha').get(0) : $('div').find('#captcha').get(0);
+    SlidingVerificationCode.init(eles, function () {
+      document.getElementById('msg').innerHTML = '验证成功！'
+    }, function () {
+      this.verify = false;
+      document.getElementById('msg').innerHTML = '验证失败！'
+    })
+  }
+
+  ngOnInit() {
+    this.inits();
+  }
+
 
   login() {
-    this.messages = ["登录成功，即将进入系统！"];
-    setTimeout(() => {
-      this.router.navigate(["/pages/dashboard"]);
-    }, 3000);
+    if (document.getElementById('msg').innerHTML == '' || document.getElementById('msg').innerHTML == '验证失败！'|| document.getElementById('msg').innerHTML == '请先完成验证!') {
+      document.getElementById('msg').innerHTML = '请先完成验证'
+    } else {
+      this.messages = ["登录成功，即将进入系统！"];
+      setTimeout(() => {
+        this.router.navigate(["/pages/dashboard"]);
+      }, 3000);
+    }
+
   }
 }
